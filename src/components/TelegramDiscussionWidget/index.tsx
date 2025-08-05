50ae8a9c6bc70b96ba7fe014d9ab4c547a4c53e8
lehakurshev <lehakurshev@mail.ru>
2025-08-05 13:10:34 +0500

    compose.yaml fix

--- src/components/TelegramDiscussionWidget/index.tsx
+++ src/components/TelegramDiscussionWidget/index.tsx
@@ -1,40 +1,30 @@
 import { TelegramPostWidget } from '@baranov-guru/react-telegram-widgets';
 import React from 'react';
-import styles from './index.module.scss';
 import { Grid } from '@mui/material';
 
 const TelegramDiscussionWidget: React.FC = () => {
   return (
-    <div style={{ backgroundColor: '#c8dcee', paddingBottom: '50px'}}>
-    <Grid container spacing={1}>
-      <Grid item xs={12} sm={6} md={3} style={{ marginLeft: '50px' }}>
-        <TelegramPostWidget
-          post='cvision_course/390'
-          width='120%' // Width для адаптивности внутри Grid
-          dark={false}
-          onLoad={() => console.log('Post loaded successfully!')}
-          onError={error => console.error('Failed to load post:', error)}
-        />
+    <div style={{ backgroundColor: '#c8dcee', height: '2000px'}}>
+      <Grid container spacing={1} style={{ backgroundColor: '#c8dcee', height: '100%'}}>
+        <Grid item xs={12} sm={6} md={3} style={{ marginLeft: '50px' }}>
+          <iframe 
+            style={{border: 'none', height: '200%', width: '100%', borderRadius: '10px' }} 
+            src={'https://t.me/cvision_course/390?embed=1'}> 
+          </iframe>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3} style={{ marginLeft: '90px' }}>
+          <iframe 
+            style={{border: 'none', height: '200%', width: '100%', borderRadius: '10px' }} 
+            src={'https://t.me/cvision_course/372?embed=1'}> 
+          </iframe>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3} style={{ marginLeft: '90px' }}>
+          <iframe 
+            style={{border: 'none', height: '200%', width: '100%', borderRadius: '10px' }} 
+            src={'https://t.me/cvision_course/371?embed=1'}> 
+          </iframe>
+        </Grid>
       </Grid>
-            <Grid item xs={12} sm={6} md={3} style={{ marginLeft: '90px' }}>
-        <TelegramPostWidget
-          post='cvision_course/372'
-          width='120%'
-          dark={false}
-          onLoad={() => console.log('Post loaded successfully!')}
-          onError={error => console.error('Failed to load post:', error)}
-        />
-      </Grid>
-      <Grid item xs={12} sm={6} md={3} style={{ marginLeft: '90px' }}>
-        <TelegramPostWidget
-          post='cvision_course/371'
-          width='120%'
-          dark={false}
-          onLoad={() => console.log('Post loaded successfully!')}
-          onError={error => console.error('Failed toload post:', error)}
-        />
-      </Grid>
-    </Grid>
     </div>
   );
 };
