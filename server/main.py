4cb791c9b8fd325c948d9e1cabaa1fcc75767e44
lehakurshev <lehakurshev@mail.ru>
2025-09-01 14:58:48 +0500

    эксперемент

--- server/main.py
+++ server/main.py
@@ -253,27 +253,27 @@ async def save_description_to_sheet(id: str):
     
 
 
-# # 
-# @app.get("/get-yookassa-widget")
-# async def get_yookassa_widget():
-#     payment = Payment.create({
-#         "amount": {
-#             "value": "2.00",
-#             "currency": "RUB"
-#         },
-#         "confirmation": {
-#             "type": "embedded"
-#         },
-#         "capture": False,
-#         "description": "Заказ №72"
-#     })
+# 
+@app.get("/get-yookassa-widget")
+async def get_yookassa_widget():
+    payment = Payment.create({
+        "amount": {
+            "value": "2.00",
+            "currency": "RUB"
+        },
+        "confirmation": {
+            "type": "embedded"
+        },
+        "capture": False,
+        "description": "Заказ №72"
+    })
 
-#     # Чтение HTML-кода из файла
-#     file_path = os.path.join(os.path.dirname(__file__), 'yookassa.html')
-#     with open(file_path, 'r', encoding='utf-8') as file:
-#         html_content = file.read()
+    # Чтение HTML-кода из файла
+    file_path = os.path.join(os.path.dirname(__file__), 'yookassa.html')
+    with open(file_path, 'r', encoding='utf-8') as file:
+        html_content = file.read()
 
-#     # Замена плейсхолдера на фактический токен подтверждения
-#     html_content = html_content.replace("$$$confirmation_token$$$", payment.confirmation.confirmation_token)
+    # Замена плейсхолдера на фактический токен подтверждения
+    html_content = html_content.replace("$$$confirmation_token$$$", payment.confirmation.confirmation_token)
 
-#     return Response(content=html_content, media_type="text/html")
+    return Response(content=html_content, media_type="text/html")
